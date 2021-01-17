262e28a0ee9444e2a3aee434288b2557e4b6bd04
Emma Paris <anmolpal1999@gmail.com>
2021-01-17 23:06:33 +0530

    User repo and data base etsting done

--- SpringSecurityApp/src/main/java/com/ecommerce/User.java
+++ SpringSecurityApp/src/main/java/com/ecommerce/User.java
@@ -1,5 +1,6 @@
 package com.ecommerce;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,13 +16,13 @@ public class User {
 	
 	private String email;
     
-//    @Column(nullable = false, length = 64)
+    @Column(nullable = false, length = 64)
     private String password;
      
-//    @Column(name = "first_name", nullable = false, length = 20)
+    @Column(name = "first_name", nullable = false, length = 20)
     private String firstName;
      
-//    @Column(name = "last_name", nullable = false, length = 20)
+    @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
 
 	public Long getId() {
